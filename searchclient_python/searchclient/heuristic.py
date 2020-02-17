6ad7a3bf2647d3f10d7c7f1aff326b1d27071a6f
maysgar <100346215@alumnos.uc3m.es>
2020-02-17 19:18:02 +0100

    heuristics added

--- searchclient_python/searchclient/heuristic.py
+++ searchclient_python/searchclient/heuristic.py
@@ -1,33 +1,79 @@
 from abc import ABCMeta, abstractmethod
+from collections import deque
+from state import State
+
+heuristic = 0
 
 
 class Heuristic(metaclass=ABCMeta):
     def __init__(self, initial_state: 'State'):
         # Here's a chance to pre-process the static parts of the level.
-        pass
-    
+        ngoals = 0
+        global heuristic
+        super().__init__()
+        self.goals = deque()
+        for i in range(initial_state.MAX_ROW):
+            for j in range(initial_state.MAX_COL):
+                if initial_state.goals[i][j] is not None:
+                    ngoals += 1
+                    self.goals.append((i, j))
+
+        if ngoals > 1:
+            heuristic = 1
+
+        else:
+            heuristic = 0
+
     def h(self, state: 'State') -> 'int':
+        if heuristic == 1:
+            return self.h_goals(state)
+
+        return self.h_distance_boxes(state)
+
+    def h_goals(self, state: 'State') -> 'int':
         boxes_todo = 0
         for row in range(state.MAX_ROW):
             for col in range(state.MAX_COL):
                 if state.boxes[row][col] and state.boxes[row][col].lower() != state.goals[row][col]:
                     boxes_todo += 1
         return boxes_todo
-    
+
+    def h_distance_agent(self, state: 'State') -> 'int':
+        goals = self.goals.copy()
+        price = 0
+        for i in range(len(goals)):
+            goal = goals.pop()
+            price = price + abs(goal[0] - state.agent_row) + abs(goal[1] - state.agent_col)
+            goals.append((goal[0], goal[1]))
+        return price
+
+    def h_distance_boxes(self, state: 'State') -> 'int':
+        goals = self.goals.copy()
+        price = 0
+        for _ in range(len(goals)):
+            goal = goals.popleft()
+            for row in range(state.MAX_ROW):
+                for col in range(state.MAX_COL):
+                    if state.boxes[row][col] and state.boxes[row][col].lower() == state.goals[goal[0]][goal[1]]:
+                        price = price + abs(row - goal[0]) + abs(col - goal[1])
+        return price
+
     @abstractmethod
-    def f(self, state: 'State') -> 'int': pass
-    
+    def f(self, state: 'State') -> 'int':
+        pass
+
     @abstractmethod
-    def __repr__(self): raise NotImplementedError
+    def __repr__(self):
+        raise NotImplementedError
 
 
 class AStar(Heuristic):
     def __init__(self, initial_state: 'State'):
         super().__init__(initial_state)
-    
+
     def f(self, state: 'State') -> 'int':
-        return state.g + self.h(state)
-    
+        return state.g + self.h_distance_boxes(state)
+
     def __repr__(self):
         return 'A* evaluation'
 
@@ -36,10 +82,10 @@ class WAStar(Heuristic):
     def __init__(self, initial_state: 'State', w: 'int'):
         super().__init__(initial_state)
         self.w = w
-    
+
     def f(self, state: 'State') -> 'int':
-        return state.g + self.w * self.h(state)
-    
+        return state.g + self.w * self.h_distance_boxes(state)
+
     def __repr__(self):
         return 'WA* ({}) evaluation'.format(self.w)
 
@@ -47,10 +93,10 @@ class WAStar(Heuristic):
 class Greedy(Heuristic):
     def __init__(self, initial_state: 'State'):
         super().__init__(initial_state)
-    
+
     def f(self, state: 'State') -> 'int':
-        return self.h(state)
-    
+        return self.h_distance(state)
+
     def __repr__(self):
         return 'Greedy evaluation'
 
